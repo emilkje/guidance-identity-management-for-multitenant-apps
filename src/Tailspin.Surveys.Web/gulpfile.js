24ae2ae511189751b52c4b2135283f46da052da8
Emil Kjelsrud <emil.kjelsrud@intility.no>
2017-05-11 14:33:29 +0200

    Migrate project and fix errors

--- src/Tailspin.Surveys.Web/gulpfile.js
+++ src/Tailspin.Surveys.Web/gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require("gulp"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
-    project = require("./project.json");
+    //project = require("./project.json");
 
 var paths = {
     webroot: "./" + project.webroot + "/"
